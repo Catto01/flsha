e065f284774212165b4cd7290a6b20964ca5b7b5
Catto01 <joaopedrocatto6122@gmail.com>
2024-12-02 13:54:50 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Quanto é 7 fatorial?',
+    '7 fatorial é 5040'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente?',
+    'O maior continente é a Ásia'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 2 elevado a 7?',
+    '2 elevado a 7 é 128'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz garrafa em Inglês?',
+    'Garrafa em inglês é bottle'
+)
